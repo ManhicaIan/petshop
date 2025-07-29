8c9ffe4586d6f5e71cb4ef2e6afb7aa505a59b70
Ian Manhica <ianmanhica4@gmail.com>
2025-07-29 16:15:32 +0200

    add dotenv

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+
+require('dotenv').config()
+
 const port = process.env.PORT || 3000
+
 
 //Database Connection
 const mongoose = require('mongoose')
-const user = "teste"
-const password = "wchjIV1KFR3jax8H"
-const dbname = "petshop"
-const uri = `mongodb+srv://${user}:${password}@cluster0.ycrkdr4.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0"`
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ycrkdr4.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority&appName=Cluster0"`
 
 mongoose.connect(uri)
 .then(() => console.log("Connection Sucessful"))
@@ -30,5 +31,6 @@ app.use((req, res, next) =>{
   res.status(404).render('NotFound')
 })
 app.listen(port, () =>{
+  console.log("Server on in port: " + port)
 
 })
